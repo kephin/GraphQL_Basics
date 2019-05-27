9d5cebc4597f6390c9dfa48d990082dbe2c2e5a6
kephin <wunshon76325@gmail.com>
2019-05-27 11:05:18 +0800

    fix: add checks and fix syntax errors

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -13,7 +13,7 @@ const Mutation = {
   },
   deleteUser(parent, args, { db }, info) {
     const user = db.users.find(user => user.id === args.userId)
-    if (!user) throw Error('User not found')
+    if (!user) throw new Error('User not found')
     db.users = db.users.filter(user => user.id !== args.userId)
     db.posts = db.posts.filter(post => {
       const isMatch = post.authorId === args.userId
@@ -26,9 +26,10 @@ const Mutation = {
   updateUser(parent, args, { db }, info) {
     const { userId, data } = args
     const isEmailTaken = db.users.some(user => user.email === data.email)
-    if (isEmailTaken) throw Error('Email is taken')
+    if (isEmailTaken) throw new Error('Email is taken')
 
     const user = db.users.find(user => user.id === userId)
+    if (!user) throw new Error('User not found')
     const userIndex = db.users.indexOf(user)
     const updatedUser = { ...user, ...data }
     db.users.splice(userIndex, 1, updatedUser)
@@ -46,7 +47,7 @@ const Mutation = {
   },
   deletePost(parent, args, { db }, info) {
     const post = db.posts.find(post => post.id === args.postId)
-    if (!post) throw Error('Post not found')
+    if (!post) throw new Error('Post not found')
     db.posts = db.posts.filter(post => post.id !== args.postId)
     db.comments = db.comments.filter(comment => comment.postId !== args.postId)
     return post
@@ -54,6 +55,7 @@ const Mutation = {
   updatePost(parent, args, { db }, info) {
     const { postId, data } = args
     const post = db.posts.find(post => post.id === postId)
+    if (!post) throw new Error('Post not found')
     const index = db.posts.indexOf(post)
     const updatedPost = { ...post, ...data }
     db.posts.splice(index, 1, updatedPost)
@@ -62,7 +64,7 @@ const Mutation = {
   createComment(parent, args, { db }, info) {
     const userExists = db.users.some(user => user.id === args.comment.authorId)
     const postExists = db.posts.some(post => post.id === args.comment.postId && post.published)
-    if (!postExists) throw Error('Unable to find user or post')
+    if (!postExists) throw new Error('Unable to find user or post')
     const newComment = {
       id: uuidv4(),
       ...args.comment,
@@ -72,13 +74,14 @@ const Mutation = {
   },
   deleteComment(parent, args, { db }, info) {
     const comment = db.comments.find(comment => comment.id === args.commentId)
-    if (!comment) throw Error('Comment not found')
+    if (!comment) throw new Error('Comment not found')
     db.comments = db.comments.filter(comment => comment.id !== args.commentId)
     return comment
   },
   updateComment(parent, args, { db }, info) {
     const { commentId, data } = args
     const comment = db.comments.find(comment => comment.id === commentId)
+    if (!comment) throw new Error('Comment not found')
     const index = db.comments.indexOf(comment)
     const updatedComment = { ...comment, ...data }
     db.comments.splice(index, 1, updatedComment)
